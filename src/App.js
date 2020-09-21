f25f3e0b4bb7486cb0c32bce79428d62c93c8290
Lawrence <Lawrence4code@gmail.com>
2020-09-21 19:17:18 +0530

    chore: add basic eg of useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const Name = ({ name }) => {
+  useEffect(() => {
+    console.log('name changed');
+  }, [name]);
+  return <h2> {name} </h2>;
+};
+
 const App = () => {
+  const [name, setName] = useState('Default Name');
   return (
     <div className="App">
-      <h1> React Playground </h1>
+      <h2> ~ Change the name ~ </h2>
+      <Name name={name} />
+      <button
+        onClick={() => {
+          setName('Whatever Doe');
+        }}
+      >
+        Change to whatever
+      </button>
+      <button
+        onClick={() => {
+          setName('John Doe');
+        }}
+      >
+        Change to John
+      </button>
     </div>
   );
 };
