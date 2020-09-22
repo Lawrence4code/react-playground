46b6cbd90e049c138adffbb6a2ce061ecd322afd
Lawrence <Lawrence4code@gmail.com>
2020-09-22 18:58:57 +0530

    useState example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((count) => count + 1);
+  };
+  const decrement = () => {
+    setCount((count) => count - 1);
+  };
+  const reset = () => {
+    setCount(() => 0); // not need
+    setCount(0);
+  };
   return (
     <div className="App">
-      <h1> React Playground </h1>
+      <h1> {count}</h1>
+      <button onClick={() => increment()}> Up </button>
+      <button onClick={() => decrement()}> Down </button>
+      <button onClick={() => reset()}> Reset </button>
     </div>
   );
 };
