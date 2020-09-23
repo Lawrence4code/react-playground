140157a8b5efc60a9e34f32ef0d26c51fbca5203
Lawrence <Lawrence4code@gmail.com>
2020-09-23 16:41:42 +0530

    useRef eg two

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
   const button = useRef(null);
 
-  useEffect(() => {
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const fakeClicker = () => {
     button.current.click();
-  }, []);
+  };
+
   return (
     <div className="App">
       <h1> React Playground </h1>
-      <button
-        ref={button}
-        onClick={() => {
-          console.log('button clicked');
-        }}
-      >
-        A button
+      <h1> {count} </h1>
+      <button onClick={fakeClicker}> Fake Counter </button>
+      <button ref={button} onClick={increment}>
+        Increase count
       </button>
     </div>
   );
