32adea82826881d60e766d5d42bd4abfce5bc3dc
Lawrence <Lawrence4code@gmail.com>
2020-09-23 16:34:39 +0530

    useRef eg one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
 
 const App = () => {
+  const button = useRef(null);
+
+  useEffect(() => {
+    button.current.click();
+  }, []);
   return (
     <div className="App">
       <h1> React Playground </h1>
+      <button
+        ref={button}
+        onClick={() => {
+          console.log('button clicked');
+        }}
+      >
+        A button
+      </button>
     </div>
   );
 };
